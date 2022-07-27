d9df60327942ccaaadb2fa293345582bbc2a7aea
Marlee Garcia <marlee.garcia121@gmail.com>
2022-07-27 12:45:26 -0400

    Add color buttons

--- src/Arrays.tsx
+++ src/Arrays.tsx
@@ -4,6 +4,10 @@ import './App.css';
 function ArraysEx() {
     const [colors, setColors] = useState(['red', 'orange', 'yellow']); 
 
+    function addColor(color: string): void {
+        setColors([...colors, color]);
+    }
+
 
     return (
         <>
@@ -14,6 +18,9 @@ function ArraysEx() {
                         <li>{color}</li>
                     ))}
                 </ul>
+                <button onClick={() => addColor('green')}>Add Green</button>
+                <button onClick={() => addColor('blue')}>Add Blue</button>
+                <button onClick={() => addColor('violet')}>Add Violet</button>
             </div>
         </>
     )
